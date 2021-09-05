ac6d8a252101494ad1f2ed56b70aa53b5d214b29
Litesh Samji <litesh@y7mail.com>
2021-09-05 21:53:42 +1000

    Java script - creating base map of austtralia

---
+++ dashboard/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibGl0ZXNoIiwiYSI6ImNrc3BxajQ4ejA1bzgydm12dzgxYzZxeGQifQ.DG_RnNJxI1DTtRU2YmB1nQ";
---
+++ dashboard/static/js/logic.js
@@ -0,0 +1,37 @@
+// Create map object
+var myMap = L.map("map", {
+	center: [-27, 132],
+	zoom: 5
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+	accessToken: API_KEY
+}).addTo(myMap);
+
+
+
+// Grab data with d3
+d3.json("data/aus_lga.geojson").then(function(data) {
+    // Create a GeoJSON layer containing the features array
+    // Each feature a popup describing the place and magnitude
+    L.geoJson(data,{
+        pointToLayer: function (feature, latlng) {
+            // Create a circle marker
+            return L.circleMarker(latlng, {
+                //radius: getRadius(feature.properties.mag), // different radius for different magnitude
+                //fillColor: chooseColor(feature.properties.mag), // different circle colors for different magnitude
+                color: "black",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            });
+        },
+        onEachFeature: function(feature, layer){
+            layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><span>Name: ${feature.properties.mag}</span>`)
+        }
+    }).addTo(myMap);
+});
