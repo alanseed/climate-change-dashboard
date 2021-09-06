5dc2052bfa33625c29f1e946d0e11e4563de9781
alanseed <alan.seed@gmail.com>
2021-09-06 16:58:50 +1000

    changed api url

--- dashboard/api/app.py
+++ dashboard/api/app.py
@@ -1,50 +1,50 @@
 from sqlalchemy import create_engine 
-from sqlalchemy.orm import Session
-import json 
 from api_keys import pg_key
-from flask import Flask
+from flask import Flask, request, render_template
 
 url = f"postgresql://postgres:{pg_key}@localhost:5432/Climate_DB" 
 engine = create_engine(url) 
 conn = engine.connect()
 
 # set up the routes 
-app = Flask(__name__)
+app = Flask(__name__) 
+
+# generate the usage page 
 @app.route("/")
-def hello_world():
-  response = "<p>Usage:</p><p> /list \"table\" Where table = stations, models, rcp</p> \n"\
-    "<p> /humidity \"station_id\"</p>" 
-  return response
+def usage():
+  return render_template("climatedb.html")
 
-# return the stations with their names and locations 
-@app.route("/list <table>")
-def list(table):
-  output = []
-  if table == "stations":
-    output.clear()
-    results = conn.execute("SELECT * FROM cl_stations")
-    for station in results:
-      rec = {"station_id": station[0],
-      "station_name_long":station[1], 
-      "station_name_short":station[2], 
-      "coord":{"lat":float(station[3]), "lon":float(station[4])} }
-      output.append(rec)
-    
-  if table == "models":
-    output.clear()
-    results = conn.execute("SELECT * FROM cl_models")
-    for model in results:
-      rec = {"model_id":model[0], 
-      "model_description":model[1]}
-      output.append(rec)
-
-  if table == "rcp":
+# get the stations with their names and locations 
+@app.route("/list",methods=['GET'])
+def list():
+  if request.method == 'GET':
+    table = request.args.get("table") 
+    output = []
+    if table == "stations":
       output.clear()
-      results = conn.execute("SELECT * FROM cl_rcp")
-      for rcp in results:
-        rec = {"rcp_id":rcp[0], 
-        "rcp_description":rcp[1]}
+      results = conn.execute("SELECT * FROM cl_stations")
+      for station in results:
+        rec = {"station_id": station[0],
+        "station_name_long":station[1], 
+        "station_name_short":station[2], 
+        "coord":{"lat":float(station[3]), "lon":float(station[4])} }
         output.append(rec)
+      
+    if table == "models":
+      output.clear()
+      results = conn.execute("SELECT * FROM cl_models")
+      for model in results:
+        rec = {"model_id":model[0], 
+        "model_description":model[1]}
+        output.append(rec)
+
+    if table == "rcp":
+        output.clear()
+        results = conn.execute("SELECT * FROM cl_rcp")
+        for rcp in results:
+          rec = {"rcp_id":rcp[0], 
+          "rcp_description":rcp[1]}
+          output.append(rec)
 
   return {"results":output}
 
@@ -94,7 +94,9 @@ def get_hum(station_id):
     data.append(rec)
   return data
 
-@app.route("/humidity <name>")
-def humidity(name):
-  output = get_hum(name)
+#get the humidity data for a station_id 
+@app.route("/humidity",methods=['GET'])
+def humidity():
+  station_id = request.args.get("station_id")
+  output = get_hum(station_id)
   return {"results":output}
