ec6298b854dc108872cb11067c7cb7b2ea323ac7
Litesh Samji <litesh@y7mail.com>
2021-09-09 18:48:46 +1000

    update

--- dashboard/static/js/logic.js
+++ dashboard/static/js/logic.js
@@ -7,11 +7,10 @@ var myMap = L.map("map", {
 // Adding tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
 	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
+    maxZoom: 10,
     id: "light-v10",
 	accessToken: API_KEY
 }).addTo(myMap);
-
 
 
 // Grab data with d3
@@ -22,10 +21,8 @@ d3.json("data/aus_lga.geojson").then(function(data) {
         pointToLayer: function (feature, latlng) {
             // Create a circle marker
             return L.circleMarker(latlng, {
-                //radius: getRadius(feature.properties.mag), // different radius for different magnitude
-                //fillColor: chooseColor(feature.properties.mag), // different circle colors for different magnitude
-                color: "black",
-                weight: 1,
+                color: "white",
+                weight: 1.5,
                 opacity: 1,
                 fillOpacity: 0.8
             });
